5757eef8b0d8b07e7cb1802f88cd6c600538fea3
Nattapong <banker.z412@gmail.com>
2023-05-29 20:38:21 +0700

    create ContributionGraph and create buttom when press amount is incress

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,29 +1,64 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useState} from 'react';
-import {
-  ActivityIndicator,
-  MD2Colors,
-  useTheme,
-  TextInput,
-} from 'react-native-paper';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import {Divider, Text} from 'react-native-paper';
+
 type Props = {};
 
 const Home = (props: Props) => {
-  const theme = useTheme();
-  const [text, setText] = useState('');
+  const [amount, setAmount] = useState<number>(0);
+
+  const handleDrinked = () => {
+    setAmount(prevAmount => prevAmount + 300);
+  };
+
+  useEffect(() => {
+    console.log(`Amount changed: ${amount}`);
+  }, [amount]);
+
+  const getCircleColor = () => {
+    if (amount < 1000) {
+      return 'green';
+    } else if (amount < 2000) {
+      return 'yellow';
+    } else {
+      return 'red';
+    }
+  };
+
+  const circleColor = getCircleColor();
 
   return (
-    <View style={{backgroundColor: theme.colors.primary, flex: 1}}>
-      <Text style={{color: 'white'}}>Home</Text>
-      <TextInput
-        label="Email"
-        value={text}
-        onChangeText={text => setText(text)}
-      />
+    <View style={[styles.container]}>
+      <TouchableOpacity onPress={handleDrinked}>
+        <View style={[styles.circle, {backgroundColor: circleColor}]}>
+          <Text>{amount}</Text>
+        </View>
+      </TouchableOpacity>
+      <Divider />
+      <Text style={styles.title}>Home</Text>
+      <Divider />
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    flex: 1,
+    alignItems: 'center',
+  },
+  circle: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: 'black',
+    fontSize: 24,
+    marginTop: 16,
+  },
+});
--- screens/Statistics.tsx
+++ screens/Statistics.tsx
@@ -1,12 +1,50 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {ContributionGraph} from 'react-native-chart-kit';
 
 type Props = {};
 
+const commitsData = [
+  {date: '2023-05-18', count: 1},
+  {date: '2023-05-19', count: 2},
+  {date: '2023-05-20', count: 3},
+  {date: '2023-05-21', count: 4},
+  {date: '2023-05-22', count: 5},
+  {date: '2023-05-23', count: 2},
+  {date: '2023-05-24', count: 3},
+  {date: '2023-05-25', count: 2},
+  {date: '2023-05-26', count: 4},
+  {date: '2023-05-27', count: 2},
+  {date: '2023-05-28', count: 4},
+];
+
+const chartConfig = {
+  backgroundGradientFrom: 'rgba(30, 41, 35, 0)',
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: 'rgba(8, 19, 13, 0)',
+  backgroundGradientToOpacity: 0,
+  color: (opacity = 1) => `rgba(26, 146, 255, ${opacity})`, // Changed to blue color
+  strokeWidth: 2,
+  barPercentage: 0.5,
+};
+const screenWidth = Dimensions.get('window').width;
 const Statistics = (props: Props) => {
   return (
     <View>
       <Text>Statistics</Text>
+      <View>
+        <ContributionGraph
+          values={commitsData}
+          endDate={new Date()}
+          numDays={105}
+          width={screenWidth}
+          height={220}
+          chartConfig={chartConfig}
+          tooltipDataAttrs={{
+            strokeWidth: 1,
+          }}
+        />
+      </View>
     </View>
   );
 };
