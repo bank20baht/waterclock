c1b0a3da5948842aeca33df35c2ed03ad3248adc
Nattapong <banker.z412@gmail.com>
2023-06-01 19:28:07 +0700

    try to use Datetimepicker mode time

--- screens/Setting.tsx
+++ screens/Setting.tsx
@@ -1,12 +1,47 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, View, Button, Pressable} from 'react-native';
+import React, {useState} from 'react';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 type Props = {};
 
 const Setting = (props: Props) => {
+  const [showTimePicker, setShowTimePicker] = useState(false);
+  const [selectedTime, setSelectedTime] = useState(new Date());
+
+  const handleTimePicker = () => {
+    setShowTimePicker(true);
+  };
+
+  const handleTimeChange = (event: any, selected: any) => {
+    if (selected) {
+      setSelectedTime(selected);
+      setShowTimePicker(false);
+    } else {
+      setShowTimePicker(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>Setting</Text>
+    <View style={{flex: 1}}>
+      <Pressable
+        style={{
+          alignContent: 'flex-end',
+          flexDirection: 'row',
+          paddingRight: 5,
+        }}
+        onPress={handleTimePicker}>
+        <Text style={{paddingRight: 5, color: 'black'}}>
+          {selectedTime.toISOString()}
+        </Text>
+      </Pressable>
+      {showTimePicker && (
+        <DateTimePicker
+          mode={'time'}
+          display="default"
+          value={selectedTime}
+          onChange={handleTimeChange}
+        />
+      )}
     </View>
   );
 };
