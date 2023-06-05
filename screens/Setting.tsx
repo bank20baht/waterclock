cdd823a4701bc0f7400f14efcf83fab38fcb0107
Nattapong <banker.z412@gmail.com>
2023-06-06 00:46:40 +0700

    try to write delete time set in Setting page but dont finish yet

--- screens/Setting.tsx
+++ screens/Setting.tsx
@@ -1,15 +1,54 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, View, Text, StatusBar} from 'react-native';
-import {Button} from 'react-native-paper';
+import {StyleSheet, View, StatusBar, ScrollView} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import CardSetTime from '../components/CardSetTime';
+import notifee, {TimestampTrigger, TriggerType} from '@notifee/react-native';
+import {useNotification} from '../src/hooks/useNotification';
+import {Button} from 'react-native-paper';
 
 const Setting = () => {
   const [storedValue, setStoredValue] = useState<any>([]);
+  const {
+    displayNotification,
+    displayTriggerNotification,
+    cancelAllNotifications,
+  } = useNotification();
 
   useEffect(() => {
     getStoredValue();
   }, []);
+
+  const setTriggerFromDB = async () => {
+    try {
+      const storedData = await AsyncStorage.getItem('timeData');
+      if (storedData) {
+        const parsedData = JSON.parse(storedData);
+        const promises = parsedData.map(async (item: any) => {
+          if (item.date && typeof item.date === 'string') {
+            const parsedDate = new Date(item.date);
+            if (!isNaN(parsedDate.getTime())) {
+              const notificationTime = parsedDate.getTime() + 3000;
+              await AsyncStorage.setItem(
+                'notificationTime',
+                notificationTime.toString(),
+              );
+              return displayTriggerNotification(
+                'NotificationTitle',
+                'NotificationBody',
+                notificationTime,
+              );
+            }
+          }
+          return Promise.reject(new Error('Invalid date'));
+        });
+
+        await Promise.all(promises);
+        console.log('All notifications displayed');
+      }
+    } catch (error) {
+      console.log('Error displaying notifications:', error);
+    }
+  };
 
   const getStoredValue = async () => {
     try {
@@ -19,6 +58,30 @@ const Setting = () => {
       }
     } catch (error) {
       console.log('Error retrieving stored value:', error);
+    }
+  };
+
+  const deleteValue = async (item: any) => {
+    try {
+      const filteredItems = storedValue.filter(
+        (storedItem: any) => storedItem.date !== item.date,
+      );
+      await AsyncStorage.setItem('timeData', JSON.stringify(filteredItems));
+      setStoredValue(filteredItems);
+      /*
+      const index = storedValue.findIndex(
+        (storedItem: any) => storedItem.date === date,
+      );
+
+      if (index !== -1) {
+        const filteredItems = [...storedValue];
+        filteredItems.splice(index, 1);
+
+        await AsyncStorage.setItem('timeData', JSON.stringify(filteredItems));
+        setStoredValue(filteredItems);
+      */
+    } catch (error) {
+      console.log('Error deleting value:', error);
     }
   };
 
@@ -48,20 +111,25 @@ const Setting = () => {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#0085ff" />
-      <View style={styles.buttonContainer}>
-        <Button
-          theme={{colors: {primary: '#0085ff', outline: '#0085ff'}}}
-          mode={'contained'}
-          onPress={saveTimeToDB}
-          icon={'plus'}>
-          Add
+      <ScrollView>
+        <View style={styles.buttonContainer}>
+          <Button onPress={saveTimeToDB}>"Create Trigger Notification"</Button>
+        </View>
+        <Button onPress={setTriggerFromDB} mode={'contained'}>
+          Test triger noti
         </Button>
-      </View>
-      <View style={styles.cardContainer}>
-        {storedValue.map((item: any, index: any) => (
-          <CardSetTime date={item.date} switchOn={item.switchOn} key={index} />
-        ))}
-      </View>
+        <View style={styles.cardContainer}>
+          {storedValue.map((item: any, index: any) => (
+            <CardSetTime
+              id={index}
+              date={item.date}
+              switchOn={item.switchOn}
+              key={index}
+              onDelete={() => deleteValue(item)}
+            />
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -78,15 +146,6 @@ const styles = StyleSheet.create({
   cardContainer: {
     marginVertical: 5,
     justifyContent: 'center',
-  },
-  rowContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: 10,
-  },
-  timeText: {
-    color: 'black',
   },
 });
 
